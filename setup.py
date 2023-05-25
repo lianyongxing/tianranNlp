8e0de55cd823075af3b86d568aa5358a07f72874
tianran <tianran@xiaohongshu.com>
2023-05-25 16:00:34 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 5/25/23 3:13 PM
+# @Author  : LIANYONGXING
+# @FileName: setup.py
+
+from setuptools import setup, find_packages
+
+tianrannlp_packages = find_packages()
+
+if 'test' in tianrannlp_packages:
+    tianrannlp_packages.remove('test')
+
+setup(
+    name='tianrannlp',
+    version='0.0.2',
+    author='tianran',
+    author_email='512796933@qq.com',
+    url='https://github.com/lianyongxing/',
+    description=u'文本处理',
+    # install_requires=['flashtext', 'jieba-fast', 'tqdm'],
+    packages=tianrannlp_packages,
+    py_modules=list(),
+    # package_dir={'': 'src'},  # 必填
+    include_package_data=True
+)
+
